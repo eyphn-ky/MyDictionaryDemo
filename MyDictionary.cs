3c2a29830481c2454ff75b9b80b4e57664cff16f
Eyuphan-NET <eyuphan.kaya34@gmail.com>
2021-01-23 18:47:00 +0300

    Upload to gitHUB

---
+++ MyDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryDemo
+{
+    public class MyDictionary<TKeys, TValues>
+    {
+        TKeys[] _tKeys;
+        TValues[] _tValues;
+        TKeys[] _tempTKeys;
+        TValues[] _tempTValues;
+        public MyDictionary()
+        {
+            _tKeys = new TKeys[0];
+            _tValues = new TValues[0];
+        }
+        public void Add(TKeys key , TValues value)
+        {
+            _tempTKeys = _tKeys;
+            _tempTValues = _tValues;
+            _tKeys = new TKeys[_tKeys.Length + 1];
+            _tValues = new TValues[_tValues.Length + 1];
+            for(int i =0;i<_tempTKeys.Length;i++)
+            {
+                _tKeys[i] = _tempTKeys[i];
+                _tValues[i] = _tempTValues[i];
+
+            }
+            _tKeys[_tKeys.Length - 1] = key;
+            _tValues[_tValues.Length - 1] = value;
+        }
+
+       
+        public int Count {
+            get
+            {
+                return _tKeys.Length;
+            } 
+        
+        }
+     
+        public void GetAll()
+        {
+            for(int i=0;i<_tKeys.Length;i++)
+            {
+                Console.WriteLine(_tKeys[i]+" : "+ _tValues[i]);
+            }
+        }
+      
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ilPlaka = new MyDictionary<int, string>();
+            ilPlaka.Add(1, "Adana");
+            ilPlaka.Add(34, "İstanbul");
+            ilPlaka.Add(36, "Kars");
+            ilPlaka.Add(44, "Malatya");
+
+
+
+            Console.WriteLine("Eleman Sayısı : " +ilPlaka.Count);             
+            ilPlaka.GetAll();
+            Console.ReadLine();
+
+
+
+
+        }
+        
+    }
+}
